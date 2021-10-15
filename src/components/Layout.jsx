0fd20c39c1c1537e20525c43f60226cd473fa62f
trungduc228 <trungduc22820022gmail.com>
2021-10-16 00:10:03 +0700

    first commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Header from './Header'
 import Routes from '../routes/routes'
 import Footer from './Footer'
 import Home from '../pages/Home'
 const Layout = () => {
     return (
-        <BrowserRouter>
+        < HashRouter>
             <div>
                 <Header />
                 <Routes />
                 <Footer />
             </div>
-        </BrowserRouter>
+        </ HashRouter>
     )
 }
 
